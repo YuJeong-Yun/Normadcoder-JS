7d7692995743a7a63f93ffa6e844c14277724358
YYJ <yunyj99@naver.com>
2022-02-21 03:19:02 +0900

    2.15 conditionals

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-function plus(a, b) {
-  console.log(a + b);
-}
-function divide(a, b) {
-  console.log(a / b)
-}
+const age = parseInt(prompt("How old are you?") );
 
-const player = {
-  name: "YJ",
-  sayHello: function(otherPersonsName) {
-    console.log("hello " + otherPersonsName + " nice to meet you!")
-  }
+if(isNaN(age) || age < 0) {
+  console.log("Please wirte a real positive number");
+} else if(age < 18) {
+  console.log("You are too young.");
+} else if(age >= 18 && age <= 50) {
+  console.log("You can drink.");
+} else if(age > 50 && age <= 80) {
+  console.log("You should exercise");
+} else if(age > 80) {
+  console.log("You can do whatever you want.");
 }
-
-player.sayHello("yj");
-player.sayHello("jj");
-
-plus(8, 20);
-divide(50, 10);
