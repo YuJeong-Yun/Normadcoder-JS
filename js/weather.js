d02b0fdbbae8b56259138b0a743418818dcc11e3
YYJ <yunyj99@naver.com>
2022-03-03 21:32:52 +0900

    8 Add weather information

---
+++ js/weather.js
@@ -0,0 +1,16 @@
+const API_KEY = "408b3884306f1c73960cfba9bd209cfd";
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const log = position.coords.longitude;  
+  const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${log}&appid=${API_KEY}&&units=metric`;
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+  });
+}
+function onGeoError() {
+  alert("Can't find you. No weather for you.")
+}
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError );
